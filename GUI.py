d950e75493b827a6653009facce096a639610f79
sholtkamp <S.Kokon93@gmail.com>
2018-09-13 16:10:53 +0200

    added additional buttons

--- GUI.py
+++ GUI.py
@@ -5,13 +5,13 @@ from tkinter import messagebox
 window = Tk()
 window.title("Bachelorarbeit Interface")
 window.geometry('500x200')
+window.configure(background="light blue")
 
 def callPortainer():
     os.system("run_portainer.io.bat")
     
 def callODM():
     os.chdir("ODM")
-    messagebox.showinfo('ODM Reminder', 'Remember to put your images in the ODM\images directory before starting')
     os.system("odm_script.bat")
     os.chdir("..")
     
@@ -25,6 +25,11 @@ def callQgisNDVI():
     os.system("qgis_ndvi.bat")
     os.chdir("..\..")
 
+def callQgisvectorize():
+    os.chdir("QGIS\QGIS_vectorize")
+    os.system("qgis_vectorize.bat")
+    os.chdir("..\..")
+
 def callQgisNDVI_unsupervised():
     os.chdir("QGIS\QGIS_NDVI_unsupervised")
     os.system("qgis_ndvi_unsupervised.bat")
@@ -35,30 +40,41 @@ def callQgisunsupervised():
     os.system("qgis_unsupervised.bat")
     os.chdir("..\..")
 
-def callQgisvectorize():
-    os.chdir("QGIS\QGIS_vectorize")
-    os.system("qgis_vectorize.bat")
+def callQgisunsupervised_interactive():
+    os.chdir("QGIS\QGIS_unsupervised_interactive")
+    os.system("qgis_unsupervised_interactive.bat")
     os.chdir("..\..")
 
+def callWorkflow():
+    os.system("workflow_automatic.bat")
+
 portainerBtn = Button(window, text="Open Portainer.io", command = callPortainer)
-portainerBtn.grid(column=1, row=0, padx=(10, 20), pady=(10, 10))
+portainerBtn.grid(column=1, row=0, padx=(10, 0), pady=(10, 0))
 
 odmBtn = Button(window, text="Run ODM", command = callODM)
-odmBtn.grid(column=2, row=0, pady=(10, 10))
+odmBtn.grid(column=2, row=0, padx=(10, 0), pady=(10, 0))
 
 ndviBtn = Button(window, text="Run NDVI", command = callNDVI)
-ndviBtn.grid(column=2, row=1, pady=(0, 10))
+ndviBtn.grid(column=2, row=1, padx=(10, 0), pady=(10, 0))
 
 qgisBtn1 = Button(window, text="Run QGIS: NDVI", command = callQgisNDVI)
-qgisBtn1.grid (column=3, row=0, pady=(10, 10))
+qgisBtn1.grid (column=3, row=0, sticky=W, padx=(10, 0), pady=(10, 0))
 
-qgisBtn2 = Button(window, text="Run QGIS: NDVI and Classification", command = callQgisNDVI_unsupervised)
-qgisBtn2.grid (column=3, row=1, pady=(0, 10))
+qgisBtn2 = Button(window, text="Run QGIS: Vectorize", command = callQgisvectorize)
+qgisBtn2.grid (column=3, row=1, sticky=W, padx=(10, 0), pady=(10, 0))
 
-qgisBtn3 = Button(window, text="Run QGIS: Unsupervised Classification", command = callQgisunsupervised)
-qgisBtn3.grid (column=3, row=2, padx=(20, 0), pady=(0, 10))
+qgisBtn3 = Button(window, text="Run QGIS: NDVI and Classification", command = callQgisNDVI_unsupervised)
+qgisBtn3.grid (column=3, row=2, sticky=W, padx=(10, 0), pady=(10, 0))
 
-qgisBtn4 = Button(window, text="Run QGIS: Vectorize", command = callQgisvectorize)
-qgisBtn4.grid (column=3, row=3, padx=(10, 0), pady=(0, 10))
+qgisBtn4 = Button(window, text="Run QGIS: Unsupervised Classification", command = callQgisunsupervised)
+qgisBtn4.grid (column=3, row=3, sticky=W, padx=(10, 0), pady=(10, 0))
+
+qgisBtn5 = Button(window, text="Run QGIS: Unsupervised Classification (interactive)", command = callQgisunsupervised_interactive)
+qgisBtn5.grid (column=3, row=4, sticky=W, padx=(10, 0), pady=(10, 0))
+
+workflowBtn=Button(window, text="Run Wofklow", bg="lime green", command = callWorkflow)
+workflowBtn.grid(column=1, row=1, padx=(10, 0), pady=(10, 0))
+
+#messagebox.showinfo('Warning!', 'Remember to make sure the required files for script execution exist')
 
 window.mainloop()
